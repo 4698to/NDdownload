5ac68c3647249b220b057390d87a7b642ec808c4
4698to <738746223@qq.com>
2026-06-24 17:01:24 +0800

    分裂出 https://gitee.com/to4698_admin/vuetify-nd-tools-admin

--- vuetify-nd-tools-admin/src/keys/dataKey.ts
+++
@@ -1,6 +0,0 @@
-import type { InjectionKey, Ref } from 'vue'
-
-export type RequestDataKeyFn = (onVerified: () => void) => void
-
-export const requestDataKeyKey: InjectionKey<RequestDataKeyFn> = Symbol('requestDataKey')
-export const isAuthorizedKey: InjectionKey<Ref<boolean>> = Symbol('isAuthorized')
